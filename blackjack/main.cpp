7bb5d6b23b80bc14f6025e9cc8b8c69ffc15a29d
Mark Higgins <mghiggins@yahoo.com>
2012-08-28 17:56:34 +0200

    Initial Commit

---
+++ blackjack/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  blackjack
+//
+//  Created by Mark Higgins on 8/28/12.
+//  Copyright (c) 2012 Mark Higgins. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
